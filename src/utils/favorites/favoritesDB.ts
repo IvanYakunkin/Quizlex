1f9539dc823db9267a27cd50448a207e040c11ac
Ivan Yakunkin <IA.Yakunkin@gmail.com>
2026-01-16 22:01:13 +0300

    refactor: use separate function

--- src/utils/favorites/favoritesDB.ts
+++ src/utils/favorites/favoritesDB.ts
@@ -1,26 +1,18 @@
 import { Card } from "@/types/types";
+import { changeFavoriteState } from "./utils";
 
-// Change useState value
-function changeFavoriteState(setCards: React.Dispatch<React.SetStateAction<Card[]>>, wordId: number){
-    setCards(prevItems => {
-        return prevItems.map(card => (
-            card.id === wordId ? {...card, isFavorite: !card.isFavorite} : card
-        ));
-    });
-}
-
-export async function setFavoriteDB(setCards: React.Dispatch<React.SetStateAction<Card[]>>, wordId: number, moduleId: number, userEmail: string){
+export async function setFavoriteDB(setCards: React.Dispatch<React.SetStateAction<Card[]>>, wordId: number, moduleId: number, userEmail: string) {
     changeFavoriteState(setCards, wordId);
-    
+
     const favoriteResponse = await fetch("/api/favorites/", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({wordId, moduleId, userEmail}),
+        body: JSON.stringify({ wordId, moduleId, userEmail }),
     });
 
-    if(!favoriteResponse.ok){
+    if (!favoriteResponse.ok) {
         throw new Error("Failed to change card info");
     }
 }
