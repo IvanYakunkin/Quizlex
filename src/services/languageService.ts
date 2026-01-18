6f038f5d3abe0e78358bee0c75120c1ef2afbd4b
Ivan Yakunkin <IA.Yakunkin@gmail.com>
2026-01-18 21:37:22 +0300

    feat: cache for getting languages

--- src/services/languageService.ts
+++ src/services/languageService.ts
@@ -1,7 +1,14 @@
-import { prisma } from "../../lib/prisma";
+import { unstable_cache } from 'next/cache';
+import { prisma } from '../../lib/prisma';
 
-export async function getLanguages() {
-    const languages = await prisma.language.findMany();
-
-    return languages;
-}
+export const getLanguages = unstable_cache(
+    async () => {
+        const languages = await prisma.language.findMany();
+        return languages;
+    },
+    ['languages-list'],
+    {
+        revalidate: 86400,
+        tags: ['languages']
+    }
+);
