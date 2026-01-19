54fd95f95917ab89b2d734cff7f0d81588164eeb
Ivan Yakunkin <IA.Yakunkin@gmail.com>
2026-01-19 22:44:53 +0300

    update: icon for cards-learning

--- src/app/module/components/Module/LearningTypes.tsx
+++ src/app/module/components/Module/LearningTypes.tsx
@@ -8,7 +8,7 @@ export const LearningTypes = () => {
 
     return (
         <div className={styles.learningTypes}>
-            <Link href={id ? `/learn/module/${id}/cards` : "/learn/cards"} className={styles.learningBtn}> <Image src="/images/file.png" width={50} height={50} alt="Cards" />Cards</Link>
+            <Link href={id ? `/learn/module/${id}/cards` : "/learn/cards"} className={styles.learningBtn}> <Image src="/images/cards-white.png" width={50} height={50} alt="Cards" />Cards</Link>
             <Link href={id ? `/learn/module/${id}/test` : "/learn/test"} className={styles.learningBtn}> <Image src="/images/file.png" width={50} height={50} alt="Test" />Test</Link>
             <Link href={id ? `/learn/module/${id}/writing` : "/learn/writing"} className={styles.learningBtn}><Image src="/images/edit.png" width={50} height={50} alt="Writing" />Writing</Link>
             <Link href={"#"} className={styles.learningBtn}><Image src="/images/choose.png" width={50} height={50} alt="Match" />Match</Link>
