794fce7fd776f380cb24c50278ddeab708844ab1
Ivan Yakunkin <IA.Yakunkin@gmail.com>
2026-01-21 15:21:40 +0300

    fix: update mock-cards

--- src/components/UI/Slider/Slider.test.tsx
+++ src/components/UI/Slider/Slider.test.tsx
@@ -1,6 +1,7 @@
 import { describe, expect, it, vi } from "vitest";
 import { render, screen } from '@testing-library/react';
 import { Slider, SliderRef } from "./Slider";
+import { BaseCard } from "@/types/module";
 
 vi.mock('next-auth/react', () => ({
     useSession: vi.fn(() => ({
@@ -18,9 +19,15 @@ describe('Slider Component', () => {
     const mockSetCards = vi.fn();
     const mockSetCurrentCardId = vi.fn();
 
-    const mockCards = [
-        { id: 1, term: 'Apple', definition: 'Яблоко' },
-        { id: 2, term: 'Banana', definition: 'Банан' }
+    const mockCards: BaseCard[] = [
+        { id: 1, isFavorite: false, term: 'test value 001', definition: "aa" },
+        { id: 2, isFavorite: true, term: 'b', definition: "bb" },
+        { id: 3, isFavorite: false, term: 'c', definition: "cc" },
+        { id: 4, isFavorite: false, term: 'd', definition: "dd" },
+        { id: 5, isFavorite: false, term: 'e', definition: "ee" },
+        { id: 6, isFavorite: false, term: 'f', definition: "ff" },
+        { id: 7, isFavorite: false, term: 'g', definition: "gg" },
+        { id: 8, isFavorite: false, term: 'h', definition: "hh" },
     ];
     const mockLanguages = { term: 'en', definition: 'ru' };
 
@@ -36,13 +43,13 @@ describe('Slider Component', () => {
             <Slider
                 cards={mockCards}
                 languages={mockLanguages}
-                setCards={mockSetCards}
+                changeCards={mockSetCards}
                 currentCardId={0}
                 setCurrentCardId={mockSetCurrentCardId}
                 sliderRef={mockSliderRef as unknown as React.RefObject<SliderRef>}
             />
         );
 
-        expect(screen.getByText('Apple')).toBeDefined();
+        expect(screen.getByText('test value 001')).toBeDefined();
     });
 });
