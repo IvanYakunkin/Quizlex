6bbe99173db4226de820f34997506b28b22560c3
Ivan Yakunkin <IA.Yakunkin@gmail.com>
2026-01-24 23:15:17 +0300

    feat: change font to OpenSans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,38 @@ import Header from "@/components/Header/Header";
 import { getServerSession, Session } from "next-auth";
 import Providers from "./providers";
 import { authOptions } from "@/lib/auth";
+import localFont from 'next/font/local';
 
 export const metadata: Metadata = {
   title: "Quizlex | Flashcards",
   description: "Quizlex is a free app for learning foreign words using flashcards. With this app, you can import or create a module and memorize it.",
 };
+
+const openSans = localFont({
+  src: [
+    {
+      path: "../../public/fonts/OpenSans-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/OpenSans-Italic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/OpenSans-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/OpenSans-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-open-sans",
+});
 
 export default async function RootLayout({
   children,
@@ -19,8 +46,8 @@ export default async function RootLayout({
   const session: Session | null = await getServerSession(authOptions);
 
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className={openSans.variable}>
+      <body className={openSans.className}>
         <Providers session={session}>
           <Header />
           {children}
