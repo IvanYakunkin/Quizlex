bc4ed196a800b4bb0cf767d443d3cdb5554441fd
Ivan Yakunkin <IA.Yakunkin@gmail.com>
2026-01-26 21:50:19 +0300

    update: cancel previous sound

--- src/utils/audio/playSound.ts
+++ src/utils/audio/playSound.ts
@@ -1,4 +1,5 @@
 export function playSound(word: string, language: string) {
+    window.speechSynthesis.cancel();
     const synthesis = new SpeechSynthesisUtterance(word);
     synthesis.lang = language;
     speechSynthesis.speak(synthesis);
